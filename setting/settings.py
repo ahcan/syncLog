f7737476c39684037e8160ce31e76f5f59a4f9be
huy <huy@localhost.localdomain>
2018-04-04 09:46:41 +0700

    config

---
+++ setting/settings.py
@@ -0,0 +1,24 @@
+# Thomson config
+THOMSON_HOST={
+    'thomson-hcm':
+    {
+        'user' : 'iptv_tool',
+        'passwd' : '123456',
+        'host'  :   '172.29.3.189',
+        'url' : 'http://%s/services/Maltese' % ('172.29.3.189'),
+    },
+    'thomson-hni':
+    {
+        'user' : 'iptv_tool',
+        'passwd' : '123456',
+        'host'  :   '172.29.70.189',
+        'url' : 'http://%s/services/Maltese' % ('172.29.70.189'),
+    },
+    'thomson-lab':
+    {
+        'user' : 'iptv_tool',
+        'passwd' : '123456',
+        'host'  : '172.17.5.110',
+        'url' : 'http://%s/services/Maltese' % ('172.17.5.110'),
+    }
+}
