c22d74cf49ca48e1ad04dd7f1797d029c66b9cb2
Huy Nguyen <tronghuy02@gmail.com>
2018-05-28 09:48:05 +0700

    add log for running script

--- getLog.py
+++ getLog.py
@@ -8,10 +8,19 @@ import json
 import logging, logging.config
 import time
 
+queTimeStart = RabbitQueue('time_log_start')
+queTimeEnd = RabbitQueue('time_log_end')
 def get_log(name = None):
-    # name: thomson-name
-    # return araay-dict
-    return Log(name).get_log()
+    """
+    name: thomson-name
+    return araay-dict
+    """
+    logger = logging.getLogger("log-run")
+    try:
+        return Log(name).get_log()
+    except Exception as e:
+        logger.error("get log from thomson %s"%(e))
+        return []
 
 def get_max_logID(lstLog, maxlID):
     for item in lstLog:
@@ -21,16 +30,24 @@ def get_max_logID(lstLog, maxlID):
         maxlID = item['lid'] if maxlID < item['lid'] else maxlID
     return maxlID
 
-def set_log_2_que(lstLog, queLog, timeEnd, timeStart):
+def get_arry_logs(lstLog, timeEnd, timeStart):
+    """
+    convert list log get from Thomson to array
+    """
     args = []
     for item in lstLog:
         if (item['opdate'] > timeStart and  item['opdate'] <= timeEnd) or (item['cldate'] > timeStart and item['cldate'] <= timeEnd):
             args.append(item)
-        #args.append(item)
-        # print("opdate: %s cldate: %s---start: %s end: %s"%(item['opdate'], item['cldate'], timeStart, timeEnd))
-    if len(args):
-        queLog.push_queue(json.dumps(args)) #add to queue
     return args
+
+def set_log_2_que(lstLog, queLog):
+    if len(lstLog):
+        for item in lstLog:
+            args = []
+            args.append(item)
+            #args.append(item)
+            # print("opdate: %s cldate: %s---start: %s end: %s"%(item['opdate'], item['cldate'], timeStart, timeEnd))
+            queLog.push_queue(json.dumps(args)) #add to queue
 
 def set_log_2_ryslog(args, name):
     ## name handler : thomsn-name
@@ -53,29 +70,32 @@ def set_log_2_ryslog(args, name):
         return "No log"
 
 def run(name = None, queNewest = None, queLog = None, timeStart = 0, timeEnd = 0):
-    ## name: thomson-name
+    """
+    name: thomson-name
+    """
+    logger = logging.getLogger('log-run') #write log for running scripts
     queLog = RabbitQueue('thomson_log')
     queNewest = RabbitQueue('newest_log')
     logs = get_log(name)
     # timeEnd =int(round(time.time()*1000))
-    args = set_log_2_que(logs, queLog, timeEnd, timeStart)
-    print ("host: %s"%(name))
-    print("lenght log is added:%d"%(len(args)))
-    print("lenght log is:%d"%(len(logs)))
+    args = get_arry_logs(logs, timeEnd, timeStart)
+    logger.info("host: %s"%(name))
+    logger.info("lenght log is added:%d"%(len(args)))
+    logger.info("lenght log is:%d"%(len(logs)))
     # print("%s---%s"%(timeStart, timeEnd))
     set_log_2_ryslog(args, name)
+    set_log_2_que(args, queLog)
     # timeStart = timeEnd
     # time.sleep(5)
-    print("%s finish!!"%(name))
+    logger.info("%s finish!!"%(name))
     return timeEnd
 
 def work_thread(hostname = None, queLog = None, queNewest = None, timeStart = 0):
     # while True:
     timeStart =  run(name = hostname, queNewest=queNewest, queLog=queLog, timeStart = timeStart)
-        #timeStart = timeTMP
+    #timeStart = timeTMP
     # print("%s finsh!!"%(hostname))
-queTimeStart = RabbitQueue('time_log_start')
-queTimeEnd = RabbitQueue('time_log_end')
+
 if __name__ == '__main__':
     threads = []
     #try:
@@ -87,6 +107,7 @@ if __name__ == '__main__':
     else:
         timeStart = int(time.time()*1000)
     time.sleep(2)
+    logger = logging.getLogger('log-run')
     timeEnd = int(time.time()*1000)
     queTimeStart.push_queue(str(timeEnd))
     for item in settings.THOMSON_HOST:
@@ -95,4 +116,4 @@ if __name__ == '__main__':
         thread.daemon = True
         thread.start()
         thread.join()
-    print('start: %s - end:%s'%(timeStart, timeEnd))
+    logger.info('start: %s - end:%s'%(timeStart, timeEnd))
